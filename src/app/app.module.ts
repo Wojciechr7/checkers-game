d5c609be978db91457543e59d9b0ffc67a79f57d
Wojciechr7 <hr5t5@windowslive.com>
2018-02-09 12:52:18 +0100

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ChatComponent } from './components/chat/chat.component';
 import {ChatService} from './components/chat/chat.service';
 
 
-const config: NgIoConfig = { url: 'https://safe-eyrie-80931.herokuapp.com/', options: {} };
+const config: NgIoConfig = { url: 'https://ng-checkers-game.herokuapp.com/', options: {} };
 
 
 
